7d066746f059b2c2f9e0976be3121494b7b38552
Zhongqiang Shen <linushen@ebay.com>
2022-03-20 13:59:16 +0800

    update map template

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import os.path
 import argparse
 import json
@@ -27,7 +28,27 @@ def get_urls():
     return urls
 
 
-def get_addresses():
+def get_date_range():
+    urls = get_urls()
+    dates = []
+    for url in urls:
+        r = requests.get(url, headers=headers, verify=False)
+        content = r.content.decode("utf-8")
+        html = etree.HTML(content)
+        title = html.xpath('//h1[@id="activity-name"]')[0].text
+        m = re.search(r'(\d+)月(\d+)日', title)
+        month = int(m.group(1))
+        day = int(m.group(2))
+        dt = datetime.date(year=2022, month=month, day=day)
+        dates.append(dt)
+    dates.sort()
+    start = dates[0]
+    end = dates[-1]
+    result = "{}月{}日-{}月{}日".format(start.month, start.day, end.month, end.day)
+    return result
+
+
+def export_points():
     urls = get_urls()
     addresses = []
     for url in urls:
@@ -35,8 +56,9 @@ def get_addresses():
         content = r.content.decode("utf-8")
         html = etree.HTML(content)
         title = html.xpath('//h1[@id="activity-name"]')[0].text
-        print(title)
         m = re.search(r'(\d+)月(\d+)日', title)
+        month = int(m.group(1))
+        day = int(m.group(2))
         if not m:
             raise Exception("date not found in title: {}".format(title))
         parts = extract_addresses(url)
@@ -47,10 +69,38 @@ def get_addresses():
             addresses.extend(parts)
 
     addresses = list(set(addresses))
+    print(len(addresses))
+    points = list(map(lambda x: get_address_point(x), addresses))
+    points = list(filter(lambda x: x is not None, points))
+
+    directory = os.path.dirname(__file__)
+    filepath = os.path.join(directory, "points")
+    with open(filepath, "w") as f:
+        f.write(json.dumps(points))
+
+
+def import_points():
+    directory = os.path.dirname(__file__)
+    filepath = os.path.join(directory, "points")
+    f = None
+    try:
+        f = open(filepath)
+        content = f.read()
+        points = json.loads(content)
+    except:
+        return []
+    finally:
+        f.close()
+    return points
+
+
+def get_addresses():
+    points = import_points()
+    range_text = get_date_range()
     file_loader = FileSystemLoader('.')
     env = Environment(loader=file_loader)
     template = env.get_template('map_template.html')
-    output = template.render(addresses=addresses)
+    output = template.render(points=points, range_text=range_text)
     with open("covid-map.html", "w") as f:
         f.write(output)
 
@@ -120,8 +170,9 @@ def get_agg_addresses(url):
         texts.append(node.text)
     texts = list(filter(lambda x: x is not None and not re.search(r'终末消毒', x), texts))
     texts = list(filter(lambda x: x != '无', texts))
-    addresses = list(map(lambda x: re.sub(r'，|。|、', '', x), texts))
-    return addresses
+    texts = list(map(lambda x: re.sub(r'，|。|、', '', x), texts))
+    texts = list(filter(lambda x: x != '', texts))
+    return texts
 
 
 def get_agg_addresses_from_url(url):
@@ -136,7 +187,6 @@ def get_agg_addresses_from_url(url):
     texts = list(filter(lambda x: x is not None and not re.search(r'终末消毒', x), texts))
     texts = list(filter(lambda x: x != '无', texts))
     addresses = list(map(lambda x: re.sub(r'，|。|、', '', x), texts))
-    print(addresses)
     title = html.xpath('//h1[@id="activity-name"]/text()')[0]
     m = re.search(r'(\d+)月(\d+)日', title)
     mon = int(m.group(1))
@@ -152,14 +202,17 @@ def get_agg_addresses_from_url(url):
 
 def get_address_point(address):
     key = "ILZBZ-6UOEO-O27WF-SA3PG-OH2NS-FLF4Z"
-    url = "https://apis.map.qq.com/ws/geocoder/v1/?address={}&region={}&key={}".format(address, "上海", key)
+    city_address = "上海市" + address
+    url = "https://apis.map.qq.com/ws/geocoder/v1/?address={}&region={}&key={}".format(city_address, "上海", key)
     r = requests.get(url, headers=headers, verify=False)
     content = r.content.decode("utf-8")
     resp = json.loads(content)
     if resp["status"] != 0:
-        raise Exception("error getting point for {}".format(address))
+        print("error getting point for {} due to {}".format(city_address, resp["message"]))
+        return None
     lng = resp["result"]["location"]["lng"]
     lat = resp["result"]["location"]["lat"]
+    time.sleep(0.5)
     return {"lng": lng, "lat": lat, "address": address}
 
 
@@ -168,6 +221,7 @@ if __name__ == "__main__":
     parser.add_argument("--date", type=str, default=None, help="e.g. 2022-01-04")
     parser.add_argument("--url", type=str, default=None, help="e.g. https://")
     parser.add_argument("--urlagg", type=str, default=None, help="e.g. https://")
+    parser.add_argument("--export", action="store_true", default=None, help="")
     args = parser.parse_args()
     if args.date:
         get_addresses_on_date(args.date)
@@ -175,5 +229,7 @@ if __name__ == "__main__":
         get_addresses_from_url(args.url)
     elif args.urlagg:
         get_agg_addresses_from_url(args.urlagg)
+    elif args.export:
+        export_points()
     else:
         get_addresses()
